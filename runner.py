d8733964d5fafd1cac239fde513d083376b72fb7
Billy Everyteen <testbench@siglabs.com>
2016-10-18 20:30:41 -0700

    targeting 7.5 seconds

--- runner.py
+++ runner.py
@@ -9,8 +9,6 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Test Runner")
     parser.add_argument('type', nargs='+', help='Must be either tx or rx')
-    # parser.add_argument('positional', nargs="+", help="A positional argument")
-    # parser.add_argument('--optional', help="An optional argument")
 
     args = parser.parse_args()
 
@@ -20,11 +18,12 @@ if __name__ == '__main__':
 
     assert run_type == 'tx' or run_type == 'rx'
 
-
     if run_type == 'rx':
         commands = ['./_rx_905E6.py', './_rx_910E6.py', './_rx_915E6.py', './_rx_920E6.py', './_rx_923E6.py']
     if run_type == 'tx':
         commands = ['./_tx_905E6.py', './_tx_910E6.py', './_tx_915E6.py', './_tx_920E6.py', './_tx_923E6.py']
+
+    target_time = 7.5
 
     runs = []
     for com in commands:
@@ -35,7 +34,16 @@ if __name__ == '__main__':
         t2 = time.time()
         print "ending at", t2
 
+        delta = t2-t1
+
+        if delta >= target_time:
+            print "ZOMG this run took TOO LONG", delta, "instead of", target_time
+        else:
+            time.sleep(target_time-delta)
+
         runs.append(t2-t1)
+
+
 
     print "all runs"
     print repr(runs)
